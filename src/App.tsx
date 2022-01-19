1a4e4bf49e9817522134132b042343a02a5052e2
ShutoYamada <stym1122@gmail.com>
2022-01-19 17:41:09 +0900

    Suspenseを試すところまで実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { Suspense } from "react";
+import "./App.css";
+import AlwaysSuspend from "./components/AlwaysSusopend";
 
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <p>
+          Suspenseの配下は全てサスペンドに巻き込まれるため、これは描画されない
+        </p>
+        <AlwaysSuspend />
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
